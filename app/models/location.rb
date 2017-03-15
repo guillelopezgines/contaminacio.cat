7cfc4b0a99ed0cc5c251c839cff6dbcef568c9d9
Guille Lopez <guillelopezgines@gmail.com>
2017-03-15 14:01:08 +0100

    Fixed bug

--- app/models/location.rb
+++ app/models/location.rb
@@ -45,11 +45,15 @@ class Location < ApplicationRecord
                   end
                   
                   Log.find_or_create_by(registered_at: datetime, pollutant_id: p.id, location_id: self.id) do |log|
-                    log.value = value.to_f
-                    log.annual_sum = previous_annual_sum + log.value
-                    log.annual_registers = previous_annual_registers + 1
+                    if log
+                      log.value = value.to_f
+                      log.annual_sum = previous_annual_sum + log.value
+                      log.annual_registers = previous_annual_registers + 1
+                      puts "#{p.name} - #{self.name} - #{datetime} - #{value} - #{log.annual_sum} - #{log.annual_registers}"
+                    else
+                      puts "No log! For registered_at: #{datetime}, pollutant_id: #{p.id} and location_id: #{self.id}"
+                    end
                   end
-                  puts "#{p.name} - #{self.name} - #{datetime} - #{value} - #{log.annual_sum} - #{log.annual_registers}"
                 end
               end
             end
